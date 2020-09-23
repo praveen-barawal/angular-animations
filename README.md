13d56541abbdb43e08c258fa08c7c428e5ea83ff
praveen-barawal <praveen.in.7@gmail.com>
2020-09-23 14:45:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-ponfcq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-ponfcq)
---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MdToolbarModule,
+  MdTabsModule,
+  MdButtonModule,
+  MdInputModule,
+  MdDatepickerModule,
+  MdNativeDateModule,
+  MdCheckboxModule,
+  MdRadioModule,
+  MatSlideToggleModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule ,MatSlideToggleModule],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
