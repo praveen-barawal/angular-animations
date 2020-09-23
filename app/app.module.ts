d2dba47b34383b02e004847e086c18b9dfa5a9f1
praveen-barawal <praveen.in.7@gmail.com>
2020-09-23 15:50:31 +0530

    4 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
   MdToolbarModule,
   MdTabsModule,
   MdButtonModule,
@@ -10,16 +41,67 @@ import {
   MdDatepickerModule,
   MdNativeDateModule,
   MdCheckboxModule,
-  MdRadioModule,
-  MatSlideToggleModule} from '@angular/material';
+  MdRadioModule
+} from "@angular/material";
 
-import { AppComponent } from './app.component';
-import { SwitchComponent } from './toggle-implementation/switch.component';
-import { ToggleComponent } from './toggle/toggle.component';
+import { AppComponent } from "./app.component";
+import { SwitchComponent } from "./toggle-implementation/switch.component";
+import { ToggleComponent } from "./toggle/toggle.component";
+import { CdkTableModule } from "@angular/cdk/table";
+import { HttpModule } from "@angular/http";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule ,MatSlideToggleModule],
-  declarations: [ AppComponent, SwitchComponent, ToggleComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
+    MdToolbarModule,
+    MdTabsModule,
+    MdButtonModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
+  ],
+  declarations: [AppComponent, SwitchComponent, ToggleComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- app/toggle-implementation/switch.component.ts
+++ app/toggle-implementation/switch.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'switch',
@@ -7,4 +8,34 @@ import { Component, Input } from '@angular/core';
 export class SwitchComponent  {
   @Input() on: boolean;
   @Input() className: string;
+
+   private showProgress = false;
+  private showText = 'Devices Off';
+  isChecked = false;
+  canShowWarn = true;
+  canShowPrimary = false;
+  colorClass = 'warn';
+
+  showTextAndLoader(showText: string){
+    setTimeout(()=>{        
+       this.showText = showText;
+       this.showProgress = false;
+    }, 1000);
+  }
+
+//Function to set Loader, change text and color change on toggle button click.
+  onClick(){
+     this.showProgress = true;
+     this.showText = 'Please Wait...';
+
+     if(this.isChecked){
+       this.colorClass = 'primary';
+       this.showTextAndLoader('Devices On');
+     }
+     else{
+       this.colorClass = 'warn';
+       this.showTextAndLoader('Devices Off');
+     }
+  }
+ 
 }
